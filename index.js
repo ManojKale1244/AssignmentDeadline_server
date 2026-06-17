1c6c6dd6f279703a6a323c1794ebaf346b885bcd
Manoj  Kale <manojkale187@gmail.com>
2026-06-17 21:30:42 +0530

    Teacher and Admin search bar

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ app.use('/api/notifications', require('./routes/notificationRoutes'))
 app.use('/api/admin', require('./routes/adminRoutes'))
 app.use('/api/teacher', require('./routes/teacherRoutes'))
 app.use('/api/student', require('./routes/studentRoutes'))
+app.use('/api/search', require('./routes/searchRoutes'))
 
 app.use(errorHandler)
 
